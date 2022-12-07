27267f8041cf295835279286325a6f8ed0d8aa73
HarshaSalih <harshasalih2000@gmail.com>
2022-12-07 20:22:15 +0530

    adminlogin commits

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+  username=""
+  password=""
+
+  readValues=()=>
+  {
+    let data:any={"username":this.username,"password":this.password}
+    console.log(data)
+    if (this.username=="admin" && this.password=="12345") {
+      alert("Valid login")
+      
+    } else {
+      alert("Invalid credential")
+      
+    }
+  }
+
+}
